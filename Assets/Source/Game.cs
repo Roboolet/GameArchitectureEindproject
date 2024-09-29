6e0a446a3750f9be63fc5bf1dc9617deccad28c3
Lea <leavdcruijsen@gmail.com>
2024-09-29 13:26:26 +0200

    Added project

---
+++ Assets/Source/Game.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
